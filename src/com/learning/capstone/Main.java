de46b1180953e940ddff56fb1bff4536f324c55c
Kramclam <kennethramclam@gmail.com>
2022-09-01 09:52:26 -0400

    main method setup

--- src/com/learning/capstone/Main.java
+++ src/com/learning/capstone/Main.java
@@ -4,6 +4,30 @@ import org.json.simple.JSONObject;
 public class Main {
     public static void main(String[] args) {
         Location locations = new Location();
+        /**
+         * Introduction to the game
+         * TODO:
+         * 1. game story intro printed out on terminal, story.gameIntro()
+         * 2. PROMPT USER to put the hero name the would like to use
+         * 3. set character attributes (currentChapter, currentItems, currentPosition, etc.)
+         * 4. create Hub object and instantiate its values.
+        * */
+
+        // (while true) loop should represent the hub
+        while(true){
+            /**
+             * TODO:
+             * 1. check if currentChapter is equal to chapter 1
+             *     1a. create Mission Object and instantiate its values
+             *     1b. Welcome user to the Hub (crash landing site?) explain
+             *          their surroundings, near by npc's, and to check for new missions.
+             *     1c. Hub.showAllMenus()
+         *         1d. PROMPT USER
+             */
+
+
+
+        }
 
     }
 }
