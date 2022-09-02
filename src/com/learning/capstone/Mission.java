ab56966f44a2bbdc7d0f8488c9202f4da510d803
Kramclam <kennethramclam@gmail.com>
2022-09-02 12:23:24 -0400

    AB#259, AB#258, AB#261 quit, help, command words

--- src/com/learning/capstone/Mission.java
+++ src/com/learning/capstone/Mission.java
@@ -1,4 +1,12 @@
 package com.learning.capstone;
 
 public class Mission {
+    private Location locations = new Location();
+    private Item items = new Item();;
+    private NPC npcs = new NPC();
+    private Hero hero = new Hero();
+
+
+
+
 }
--- src/com/learning/capstone/Story.java
+++ src/com/learning/capstone/Story.java
@@ -15,4 +15,16 @@ public class Story {
     public static void alienOutpost(){
         System.out.println("You find yourself in a strange field. everything has been cleared out and all that stands before you is an unknown tower");
     }
+
+    public static void hubHelp(){
+        System.out.println("Use the menu items to see what can be done while you are in the Hub");
+        System.out.println("-Items: Check to see what your hero has on their person. Items can be found in missions.");
+        System.out.println("-Missions: A list of available missions you can go on to help find new items and unlock storyline.");
+    }
+
+    public static void missionHelp(){
+        System.out.println("Use the menu items to see what can be done while you are in the Hub");
+        System.out.println("-Items: Check to see what your hero has on their person. Items can be found in missions.");
+        System.out.println("-Missions: A list of available missions you can go on to help find new items and unlock storyline.");
+    }
 }
