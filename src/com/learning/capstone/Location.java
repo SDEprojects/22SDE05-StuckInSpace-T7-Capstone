5d11dfbed5c93c3ed30ffc84260049854d2042fe
Kramclam <kennethramclam@gmail.com>
2022-09-06 15:11:09 -0400

    AB#265, updated mission and hub functions, movement and locations

--- src/com/learning/capstone/Location.java
+++ src/com/learning/capstone/Location.java
@@ -5,10 +5,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import javax.swing.text.html.HTMLDocument;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Location {
     private final JSONArray locDict;
@@ -21,13 +18,11 @@ public class Location {
     private JSONArray readLocationFile() {
         //JSON parser object to parse read file
         JSONParser jsonParser = new JSONParser();
-
+        JSONArray locationDict = null;
         try (FileReader reader = new FileReader("location_dictionary.json")) {
             Object obj = jsonParser.parse(reader);
 
-            JSONArray locDict = (JSONArray) obj;
-            //locDict.forEach((loc) -> parseLocationObject((JSONObject) loc, "chapter 1"));
-            //System.out.println(locDict);
+            locationDict = (JSONArray) obj;
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -36,7 +31,7 @@ public class Location {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return locDict;
+        return locationDict;
     }
 
     public void parseLocationObject(JSONObject location, String chapter, String start)
@@ -55,9 +50,5 @@ public class Location {
         return locDict;
     }
 
-    public boolean isExitAvailable(String exit){
-        boolean isExit = false;
-        return isExit;
-    }
 }
 
