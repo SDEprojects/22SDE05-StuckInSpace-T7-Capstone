47772e45c82e0e49e77b6f4d557b3aee7a134e20
Neten Shrestha <netenshrestha@gmail.com>
2022-09-28 19:12:40 -0400

    AB#1225 9/28 godMode test.

--- test/com/learning/client/ItemHandlerTest.java
+++ test/com/learning/client/ItemHandlerTest.java
@@ -1,14 +1,30 @@
 package com.learning.client;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static com.learning.client.GameManager.hub;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ItemHandlerTest {
 
-//    @Test
-//    void getInventoryButtonsList() {
-//        ArrayList<String> itemsTest =
-//    }
-//
+    @Test
+    void getInventoryButtonsList() {
+        //HubSpot hubTest = new HubSpot();
+        ArrayList<String> expectedArrayList = new ArrayList<>();
+        expectedArrayList.add("boots");
+        //expectedArrayList.add("TM");
+        hub.initiateItemLocationList();
+        ArrayList<String> actualArrayList=ItemHandler.getInventoryButtonsList("hallway");
+        assertEquals(expectedArrayList, actualArrayList);
 
-//    @Test
-//    void updateInventoryCount() {
-//    }
+
+    }
+
+
+    @Test
+    void updateInventoryCount() {
+        ArrayList<String> updateInventoryTest= hub.item.getBackpackList();
+    }
 }
